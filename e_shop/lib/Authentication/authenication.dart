ab7f5fc792300fcc2fbc34735b092e108c7cb7e7
JavierGold <567498jav@gmail.com>
2021-10-29 13:56:54 -0500

    AppBar

--- e_shop/lib/Authentication/authenication.dart
+++ e_shop/lib/Authentication/authenication.dart
@@ -14,6 +14,61 @@ class _AuthenticScreenState extends State<AuthenticScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: new BoxDecoration(
+              gradient: new LinearGradient(
+                  colors: [Colors.white30, Colors.lightBlueAccent],
+                  begin: const FractionalOffset(0.0, 0.0),
+                  end: const FractionalOffset(1.0, 0.0),
+                  stops: [0.0,1.0],
+                  tileMode: TileMode.clamp,
+              ),
+            ),
+          ),
+          title: Text(
+            "Tienda Online",
+            style: TextStyle(
+              fontSize: 55.0,
+              color: Colors.white,
+              fontFamily: "Signatra",
+            ),
+          ),
+          centerTitle: true,
+          bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.lock, color: Colors.white),
+                  text: "Login",
+                ),
+                Tab(
+                  icon: Icon(Icons.person, color: Colors.white),
+                  text: "Registro",
+                ),
+              ],
+            indicatorColor: Colors.white38,
+            indicatorWeight: 5.0,
+          ),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: new LinearGradient(
+                colors: [Colors.blue, Colors.lightBlueAccent],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                tileMode: TileMode.clamp,
+              )
+          ),
+          child: TabBarView(
+            children: [
+              Login(),
+              Register(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
